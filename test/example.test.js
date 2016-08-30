6851dc172dae3ae15bbec90df782b6e919edc17d
Matt Glover <matthewglover@me.com>
2016-08-30 16:50:35 +0100

    Setup test .babelrc

--- test/example.test.js
+++ test/example.test.js
@@ -3,3 +3,14 @@ import test from 'ava';
 test('True is true', t => {
   t.ok(true);
 });
+
+
+const delayed = () =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(10), 1000);
+  });
+
+test('Async tests are cool', async t => {
+  const expected = await delayed();
+  t.is(expected, 10);
+});
