53543c98209d4c895136ad6fca48e6290169d750
Matt Glover <matthewglover@me.com>
2016-09-19 10:19:31 +0100

    fix stories
    CLOSES #2

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import { storiesOf, action } from '@kadira/storybook';
-
 
 storiesOf('Button', module)
   .add('with text', () =>
---
+++ stories/tap_events.js
@@ -0,0 +1,5 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
