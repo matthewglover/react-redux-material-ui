4ebca474a6ef9033b9b5dceedef7a53c3ccb93d8
Matt Glover <matthewglover@me.com>
2016-09-19 12:50:59 +0100

    improve storybook linter

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
-/* eslint-disable global-require */
+ /* eslint-disable global-require */
 import { configure } from '@kadira/storybook';
 
 const loadStories = () => {
   require('../stories/index');
   require('../stories/icon_menu');
+  require('../stories/square');
 };
 
 configure(loadStories, module);
